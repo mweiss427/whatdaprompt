87b5c2047d12090c0ef72853d8c7481227100d67
Matt Weiss <mweiss@northwindstech.com>
2023-03-19 09:53:25 -0500

    more css changes, added the prompt feature and hiding the prompt

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
-import "./App.css"
+import "./App.css";
 
 function App() {
   const [prompt, setPrompt] = useState("");
   const [imageData, setImageData] = useState(null);
+  const [showPrompt, setShowPrompt] = useState(false);
 
   async function handleSubmit(event) {
     event.preventDefault();
-  
+
     const response = await fetch("/api/generate-image", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ prompt, num_images: 1, size: [256, 256] }),
+      body: JSON.stringify({ prompt, num_images: 1, size: [512, 512] }),
     });
-  
+
     const data = await response.json();
     console.log("Image data:", data);
-  
+
     if (data && data.data && data.data[0] && data.data[0].url) {
       setImageData(data.data[0].url);
     } else {
@@ -26,16 +27,17 @@ function App() {
 
   return (
     <div className="App">
-      <header>
+      <header className="App-header">
         <h1>Guess the Prompt</h1>
       </header>
       <form onSubmit={handleSubmit}>
         <label>
           Prompt:
           <input
-            type="text"
+            type="password"
             value={prompt}
             onChange={(event) => setPrompt(event.target.value)}
+            placeholder="Enter your prompt"
           />
         </label>
         <button type="submit">Generate Image</button>
@@ -43,7 +45,11 @@ function App() {
 
       {imageData && (
         <div className="image-container">
-          <img src={imageData} alt="Generated Image" />
+          <img src={imageData} alt="Generated Image" className="image" />
+          <button className="show-prompt" onClick={() => setShowPrompt(!showPrompt)}>
+            {showPrompt ? "Hide Prompt" : "Show Prompt"}
+          </button>
+          {showPrompt && <p className="prompt">{prompt}</p>}
         </div>
       )}
     </div>
