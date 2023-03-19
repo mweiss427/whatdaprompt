0b75fa6f28c72b1ee9f3ba5db9680e846aa06821
Matt Weiss <mweiss@northwindstech.com>
2023-03-19 09:45:07 -0500

    working of image generation

---
+++ server.js
@@ -0,0 +1,48 @@
+const fetch = require("node-fetch");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const API_KEY = "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";
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/generate-image", async (req, res) => {
+  try {
+    const { prompt, num_images, size } = req.body;
+
+    const openaiResponse = await fetch("https://api.openai.com/v1/images/generations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "image-alpha-001",
+        prompt,
+        num_images,
+        size: `${size[0]}x${size[1]}`, // Convert the size array to a string in the format "256x256"
+        response_format: "url",
+      }),
+    });
+
+    if (!openaiResponse.ok) {
+      console.error("Response received from OpenAI API:", openaiResponse);
+      const errorResponse = await openaiResponse.json();
+      console.error("Error response JSON:", errorResponse);
+      res.status(500).json({ error: "Failed to fetch image" });
+    } else {
+      const data = await openaiResponse.json();
+      res.json(data);
+    }
+  } catch (error) {
+    console.error("Error occurred during the request:", error);
+    res.status(500).json({ error: "Failed to fetch image" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+  const [imageData, setImageData] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+  
+    const response = await fetch("/api/generate-image", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ prompt, num_images: 1, size: [256, 256] }),
+    });
+  
+    const data = await response.json();
+    console.log("Image data:", data);
+  
+    if (data && data.data && data.data[0] && data.data[0].url) {
+      setImageData(data.data[0].url);
+    } else {
+      console.error("Failed to get image URL from response");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Prompt:
+          <input
+            type="text"
+            value={prompt}
+            onChange={(event) => setPrompt(event.target.value)}
+          />
+        </label>
+        <button type="submit">Generate Image</button>
+      </form>
+
+      {imageData && <img src={imageData} alt="Generated Image" />}
     </div>
   );
 }
